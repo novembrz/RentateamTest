2a92fd3736b8057134515f3b1ba51fe7f4c10f41
Dasha Kurilova <kurilovadk@gmail.com>
2021-02-16 10:55:55 +0300

    Deleted the storyboard

---
+++ RentateamTest/RentateamTest/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentateamTest
+//
+//  Created by Дарья on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
